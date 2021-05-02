7c73d054fa93e945747360f87ebe989f485ccfcd
Israel Rocha <israel.rexc@yahoo.com>
2021-05-02 17:59:39 -0300

    Rastreando os pasos

--- Robô em Grade.cpp
+++ Robô em Grade.cpp
@@ -1,20 +1,145 @@
-// Robô em Grade.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <vector>
+#include <utility>
+
+#define white -1
+#define gray 0
+#define black 1
+
+
+typedef struct {
+    std::vector<std::vector<char>> area;
+    std::vector<std::vector<int>> state; //Se foi ou nao visitado
+}graph;
+
+typedef struct std::pair<int, int> coord;
+
+void print(std::vector<std::vector<int>> map);
+void track(std::vector<std::vector<char>> map, coord pos);
+void adj(std::vector<std::pair<int, int>> neighbour);
+
+
+std::vector<coord> adjList(graph* g, coord pos) {
+    std::vector<coord> aux;
+    if (!(pos.first + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first + 1][pos.second] == white) aux.push_back(std::make_pair(pos.first + 1, pos.second));
+    }
+    
+    if (!(pos.second + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first][pos.second + 1] == white) aux.push_back(std::make_pair(pos.first, pos.second + 1));
+    }
+    
+    if (!(pos.first - 1 < 0)) {
+        if ((g->state)[pos.first - 1][pos.second] == white) aux.push_back(std::make_pair(pos.first - 1, pos.second));
+    }
+    
+    if (!(pos.second - 1 < 0)) {
+        if ((g->state)[pos.first][pos.second - 1] == white) aux.push_back(std::make_pair(pos.first, pos.second - 1));
+    }
+    return aux;
+}
+
+/*
+ *
+ * flag => diz qual tipo de percurso é (1 = andar para cima e esquerda, 0 = andar somente para baixo e direita)
+ * pos => posicao do vertice no grafo (linha == x, coluna == y)
+ */
+int deepSearch(graph* g, coord pos, int flag) {
+    int result1 = 0, result2 = 0;
+    //pintaG
+    g->state[pos.first][pos.second] = gray;
+    
+
+    std::vector<coord> list;
+    // Monta lista de adjacencia
+    list = adjList(g, pos);
+
+    // x é a nova posicao para se mover
+    for (std::pair<int,int>x : list) {
+        adj(list);
+        std::cout << x.first << ' ' << x.second << std::endl;
+        track(g->area, pos);
+        //Se andou para tras ou para cima
+        if ((x.first < pos.first) || (x.second < pos.second)) deepSearch(g, x, 1);
+        else deepSearch(g, x, flag);  
+    }
+
+    int n = g->state[0].size()-1;
+    // Se encontrou a saida
+    if (pos == std::make_pair(n, n)) {
+        std::cout << "Encontrou a saida" << std::endl;
+        if (flag) result1++;
+        else result2++;
+    } 
+
+    g->state[pos.first][pos.second] = white;
+    return result1;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 0, i = 0, j = 0;
+    char aux;
+
+    std::cin >> n;
+    std::vector<std::vector<char>> map(n); // mapa
+    std::vector<std::vector<int>> col(n); // cores dos vertices
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            std::cin >> aux;
+            map[i].push_back(aux);
+            if (aux == '#')col[i].push_back(black);
+            else col[i].push_back(white);
+
+        }    
+    }
+    graph g;
+    g.area = map;
+    g.state = col;
+
+    //adjList(&g, std::make_pair(4,4));
+    std::cout << deepSearch(&g, std::make_pair(0, 0), 0);
+    
+   
+   //print(col);
+
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
 
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
+void track(std::vector<std::vector<char>> map, coord pos) {
+    int i = 0, j = 0;
+    int n = map[0].size();
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (pos == std::make_pair(i, j)) std::cout << "x" << ' ';
+            else std::cout << map[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+}
+
+
+
+void print(std::vector<std::vector<int>> map) {
+    int i = 0, j = 0;
+    int n = map[0].size();
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            
+            std::cout << map[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void adj(std::vector<std::pair<int, int>> neighbour) {
+    std::cout << "Adjacent list: ";
+    for (std::pair<int, int> x : neighbour) {
+        std::cout << "[" << x.first << ", " << x.second << "]" << "||";
+    }
+    std::cout << std::endl;
+}
