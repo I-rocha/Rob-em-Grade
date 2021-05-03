fe924a54ba4f2ff4594767bd0d0b255d9d620fe1
Israel Rocha <israel.rexc@yahoo.com>
2021-05-03 18:06:33 -0300

    LargeSearch implementado

--- Robô em Grade.cpp
+++ Robô em Grade.cpp
@@ -15,23 +15,21 @@ typedef struct {
 
 typedef struct std::pair<int, int> coord;
 
-void print(std::vector<std::vector<int>> map);
-void track(std::vector<std::vector<char>> map, coord pos);
-void adj(std::vector<std::pair<int, int>> neighbour);
-
 
 std::vector<coord> adjList(graph* g, coord pos, int fg) {
     std::vector<coord> aux;
-    if (fg == 0) {
-        if (!(pos.first + 1 >= g->state[0].size())) {
-            if ((g->state)[pos.first + 1][pos.second] == white) aux.push_back(std::make_pair(pos.first + 1, pos.second));
-        }
-
-        if (!(pos.second + 1 >= g->state[0].size())) {
-            if ((g->state)[pos.first][pos.second + 1] == white) aux.push_back(std::make_pair(pos.first, pos.second + 1));
-        }
+    
+    
+    if (!(pos.first + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first + 1][pos.second] == white) aux.push_back(std::make_pair(pos.first + 1, pos.second));    
     }
-    else if (fg == 1) {
+
+        
+    if (!(pos.second + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first][pos.second + 1] == white) aux.push_back(std::make_pair(pos.first, pos.second + 1));
+    }
+    
+    if (fg == 1) {
         if (!(pos.first - 1 < 0)) {
             if ((g->state)[pos.first - 1][pos.second] == white) aux.push_back(std::make_pair(pos.first - 1, pos.second));
         }
@@ -41,6 +39,68 @@ std::vector<coord> adjList(graph* g, coord pos, int fg) {
         }
     }
     return aux;
+}
+
+std::vector<coord> adjListLargeSearch(graph* g, coord pos) {
+    std::vector<coord> aux;
+
+
+    if (!(pos.first + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first + 1][pos.second] == white) {
+            aux.push_back(std::make_pair(pos.first + 1, pos.second));
+            (g->state)[pos.first + 1][pos.second] = gray;
+        }
+    }
+
+
+    if (!(pos.second + 1 >= g->state[0].size())) {
+        if ((g->state)[pos.first][pos.second + 1] == white) {
+            aux.push_back(std::make_pair(pos.first, pos.second + 1));
+            (g->state)[pos.first][pos.second + 1] = gray;
+        }
+    }
+
+ 
+    if (!(pos.first - 1 < 0)) {
+        if ((g->state)[pos.first - 1][pos.second] == white) {
+            aux.push_back(std::make_pair(pos.first - 1, pos.second));
+            (g->state)[pos.first - 1][pos.second] = gray;
+        }    
+    }
+
+        
+    if (!(pos.second - 1 < 0)) {
+        if ((g->state)[pos.first][pos.second - 1] == white) {
+            aux.push_back(std::make_pair(pos.first, pos.second - 1));
+            (g->state)[pos.first][pos.second - 1] = gray;   
+        }    
+    }
+    return aux;
+}
+
+int largeSearch(graph* g, coord pos) {
+    // n == posicao final
+    int n = g->state[0].size() - 1;
+    std::vector<coord> adj;
+    
+    // Pinta estado
+    g->state[pos.first][pos.second] = black;
+
+    // Verifica se está na ultima posicao
+    if (pos == std::make_pair(n, n)) return 1;
+
+    // Cria lista de adjacencia
+    adj = adjListLargeSearch(g, pos);
+
+    // Percorre toda a vizinhança
+    for (coord x : adj) {
+
+        // Se encontrar um caminho retorna
+        if (largeSearch(g, x)) return 1;
+    }
+
+
+    return 0;
 }
 
 /*
@@ -54,10 +114,10 @@ int deepSearch(graph* g, coord pos, int flag, unsigned long* result1, unsigned l
     //pintaG
     g->state[pos.first][pos.second] = gray;
 
+    if (fg == 1 && (*result2) > 0) { return 0; }
 
     //Se encontrou saida
     if (pos == std::make_pair(n, n)) {
-        std::cout << "Encontrou a saida" << std::endl;
         if (!flag) (*result1)++;
         else (*result2)++;
         g->state[pos.first][pos.second] = white;
@@ -70,14 +130,11 @@ int deepSearch(graph* g, coord pos, int flag, unsigned long* result1, unsigned l
     list = adjList(g, pos, fg);
 
     // x é a nova posicao para se mover
-    for (std::pair<int, int>x : list) {
+    for (coord x : list) {
 
-        std::cout << x.first << ' ' << x.second << std::endl;
-        track(g->area, x);
         //Se andou para tras ou para cima
         if ((x.first < pos.first) || (x.second < pos.second)) deepSearch(g, x, 1, result1, result2, fg);
         else deepSearch(g, x, flag, result1, result2, fg);
-        adj(list);
     }
 
     g->state[pos.first][pos.second] = white;
@@ -109,61 +166,16 @@ int main()
     g.state = col;
 
     deepSearch(&g, std::make_pair(0, 0), 0, &result1, &result2, 0);
-    
+
     if (result1 != 0) {
         normalized_1 = result1 % max;
         std::cout << normalized_1;
     }
     deepSearch(&g, std::make_pair(0, 0), 0, &result1, &result2, 1);
-    if (result2 != 0) {
+    if ((result1 == 0) && (result2 != 0)) {
         std::cout << "THE GAME IS A LIE";
     }
-    else {
+    else if ((result1 == 0) && (result2 == 0)){
         std::cout << "INCONCEIVABLE";
     }
-
-    std::cout << result2;
-
-
-
-    //print(col);
-
-}
-
-
-void track(std::vector<std::vector<char>> map, coord pos) {
-    int i = 0, j = 0;
-    int n = map[0].size();
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (pos == std::make_pair(i, j)) std::cout << "x" << ' ';
-            else std::cout << map[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl << std::endl;
-}
-
-
-
-void print(std::vector<std::vector<int>> map) {
-    int i = 0, j = 0;
-    int n = map[0].size();
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-
-            std::cout << map[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
-}
-
-void adj(std::vector<std::pair<int, int>> neighbour) {
-    std::cout << "Adjacent list: ";
-    for (std::pair<int, int> x : neighbour) {
-        std::cout << "[" << x.first << ", " << x.second << "]" << "||";
-    }
-    std::cout << std::endl;
 }
