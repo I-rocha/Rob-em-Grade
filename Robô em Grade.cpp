9c1135e183023ec413090838915b4e9dad8cb0fc
Israel Rocha <israel.rexc@yahoo.com>
2021-05-03 18:24:34 -0300

    funcionando ncom busca em largura e busca em profundidade

--- Robô em Grade.cpp
+++ Robô em Grade.cpp
@@ -16,28 +16,17 @@ typedef struct {
 typedef struct std::pair<int, int> coord;
 
 
-std::vector<coord> adjList(graph* g, coord pos, int fg) {
+std::vector<coord> adjList(graph* g, coord pos) {
     std::vector<coord> aux;
-    
     
     if (!(pos.first + 1 >= g->state[0].size())) {
         if ((g->state)[pos.first + 1][pos.second] == white) aux.push_back(std::make_pair(pos.first + 1, pos.second));    
     }
-
-        
+  
     if (!(pos.second + 1 >= g->state[0].size())) {
         if ((g->state)[pos.first][pos.second + 1] == white) aux.push_back(std::make_pair(pos.first, pos.second + 1));
     }
     
-    if (fg == 1) {
-        if (!(pos.first - 1 < 0)) {
-            if ((g->state)[pos.first - 1][pos.second] == white) aux.push_back(std::make_pair(pos.first - 1, pos.second));
-        }
-
-        if (!(pos.second - 1 < 0)) {
-            if ((g->state)[pos.first][pos.second - 1] == white) aux.push_back(std::make_pair(pos.first, pos.second - 1));
-        }
-    }
     return aux;
 }
 
@@ -108,33 +97,26 @@ int largeSearch(graph* g, coord pos) {
  * flag => diz qual tipo de percurso é (1 = andar para cima e esquerda, 0 = andar somente para baixo e direita)
  * pos => posicao do vertice no grafo (linha == x, coluna == y)
  */
-int deepSearch(graph* g, coord pos, int flag, unsigned long* result1, unsigned long* result2, int fg) {
-    int n = g->state[0].size() - 1; // Ultima posicao
+int deepSearch(graph* g, coord pos, int flag, unsigned long* result) {
+    int n = g->state[0].size() - 1; // n == posicao final
 
     //pintaG
     g->state[pos.first][pos.second] = gray;
 
-    if (fg == 1 && (*result2) > 0) { return 0; }
-
     //Se encontrou saida
     if (pos == std::make_pair(n, n)) {
-        if (!flag) (*result1)++;
-        else (*result2)++;
-        g->state[pos.first][pos.second] = white;
-        return *result2;
+       (*result)++;
     }
-
 
     std::vector<coord> list;
     // Monta lista de adjacencia
-    list = adjList(g, pos, fg);
+    list = adjList(g, pos);
 
     // x é a nova posicao para se mover
     for (coord x : list) {
 
-        //Se andou para tras ou para cima
-        if ((x.first < pos.first) || (x.second < pos.second)) deepSearch(g, x, 1, result1, result2, fg);
-        else deepSearch(g, x, flag, result1, result2, fg);
+        deepSearch(g, x, flag, result);
+    
     }
 
     g->state[pos.first][pos.second] = white;
@@ -145,12 +127,13 @@ int main()
 {
     int n = 0, i = 0, j = 0;
     char aux;
-    unsigned long result1 = 0, result2 = 0, max = (pow(2.0, 31.0) - 1);
+    unsigned long result = 0, max = (pow(2.0, 31.0) - 1);
     int normalized_1 = 0, normalized_2 = 0;
 
     std::cin >> n;
     std::vector<std::vector<char>> map(n); // mapa
     std::vector<std::vector<int>> col(n); // cores dos vertices
+
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
@@ -165,17 +148,14 @@ int main()
     g.area = map;
     g.state = col;
 
-    deepSearch(&g, std::make_pair(0, 0), 0, &result1, &result2, 0);
+    deepSearch(&g, std::make_pair(0, 0), 0, &result);
 
-    if (result1 != 0) {
-        normalized_1 = result1 % max;
+    if (result != 0) {
+        normalized_1 = result % max;
         std::cout << normalized_1;
-    }
-    deepSearch(&g, std::make_pair(0, 0), 0, &result1, &result2, 1);
-    if ((result1 == 0) && (result2 != 0)) {
+    } else if(largeSearch(&g, std::make_pair(0,0)))
         std::cout << "THE GAME IS A LIE";
-    }
-    else if ((result1 == 0) && (result2 == 0)){
+    else{
         std::cout << "INCONCEIVABLE";
     }
 }
